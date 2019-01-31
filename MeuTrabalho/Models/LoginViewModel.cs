2b86078fe75132767a176d641e3a888691ec898b
Paulo Gentile <paulo_gentile@msn.com>
2019-01-31 21:20:53 -0200

    .

--- MeuTrabalho/Models/LoginViewModel.cs
+++ MeuTrabalho/Models/LoginViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MeuTrabalho.Models
+﻿namespace MeuTrabalho.Models
 {
     public class LoginViewModel
     {
